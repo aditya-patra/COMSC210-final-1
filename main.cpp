59843809a1894e70ecebf022d2de8161092b0abd
Aditya Patra <adityabpatra@gmail.com>
2025-12-08 15:26:39 -0800

    trying to figure out how to split flight path

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream file("210-final-1-FA25.txt");
+    string start_airport;
+    string end_airport;
+    string flight;
+    while(getline(file, flight)) {
+        int start = 1;
+    }
+}
